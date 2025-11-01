21fbe4812667c6f7943d6011eaa1b20dcc9658b3
Prashanth_Chanagoni <136735366+Prashanth021729@users.noreply.github.com>
2025-11-01 15:42:16 -0500

    Update storage_integration.sql

--- storage_integration.sql
+++ storage_integration.sql
@@ -3,3 +3,6 @@ CREATE STORAGE INTEGRATION snow_pipe
       STORAGE_PROVIDER = 'GCS'
       ENABLED = TRUE
       STORAGE_ALLOWED_LOCATIONS = ('gcs://<location>/');
+
+desc storage integration snow_pipe;
+-- Make sure to whitelist storage account on gcp
